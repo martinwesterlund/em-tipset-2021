51b3205c810f178cd69ade06c3d2b2213f33d0cc
Martin Westerlund <westerlund_martin@hotmail.com>
2021-06-03 22:12:24 +0200

    Add info on next match component

--- components/NextMatch.js
+++ components/NextMatch.js
@@ -1,29 +1,43 @@
-import { motion } from "framer-motion"
-
+import { motion } from "framer-motion";
 const NextMatch = ({ match }) => {
   return (
     <div className="w-full">
       {match.id != 99 ? (
         <div className="w-full h-full flex flex-col justify-center items-center">
-          <span className="text-xs md:text-lg text-em-green-dark">Nästa match</span>
+          <span className="text-xs md:text-lg text-em-green-dark">
+            Nästa match
+          </span>
+          <div className="flex w-full h-10 justify-center items-center mt-2">
+            <span className="mx-2">
+              {match.date} kl. {match.time}
+            </span>
+            
+            <img
+              className={`mx-2 h-6 ${match.channel === 'svt1' || match.channel === 'svt2' || match.channel === 'svt24' ? 'mb-2' : ''}`}
+              src={`/images/${match.channel}.svg`}
+              alt=""
+            />
+          </div>
           <div className="flex w-full items-center justify-center mt-4 px-4 text-xs md:text-lg">
             <div className="w-1/3 flex flex-col justify-center items-center">
               <motion.img
-                initial={{scale:0}}
-                animate={{scale:1, transition: {delay:1}}}
+                initial={{ scale: 0 }}
+                animate={{ scale: 1, transition: { delay: 1 } }}
                 className="w-12 h-12 rounded-full object-cover shadow-md"
                 src={`/images/flags/${match.home_team.toLowerCase()}.svg`}
                 alt=""
               />
               <span className="mt-4">{match.home_team}</span>
             </div>
-            <motion.div initial={{ height: 0 }} animate={{ height: 80, transition: { delay:1, duration:1 }}} className="w-px bg-em-green-light h-full transform mx-8">
-              
-            </motion.div>
+            <motion.div
+              initial={{ height: 0 }}
+              animate={{ height: 80, transition: { delay: 1, duration: 1 } }}
+              className="w-px bg-em-green-light h-full transform mx-8"
+            ></motion.div>
             <div className="w-1/3 flex flex-col justify-center items-center">
               <motion.img
-              initial={{scale:0}}
-              animate={{scale:1, transition: {delay:1.2}}}
+                initial={{ scale: 0 }}
+                animate={{ scale: 1, transition: { delay: 1.2 } }}
                 className="w-12 h-12 rounded-full object-cover shadow-md"
                 src={`/images/flags/${match.away_team.toLowerCase()}.svg`}
                 alt=""
@@ -32,8 +46,11 @@ const NextMatch = ({ match }) => {
             </div>
           </div>
         </div>
-      ) : <div className="w-full h-full flex flex-col justify-center items-center py-4">
-        {match.message}</div>}
+      ) : (
+        <div className="w-full h-full flex flex-col justify-center items-center py-4">
+          {match.message}
+        </div>
+      )}
     </div>
   );
 };
